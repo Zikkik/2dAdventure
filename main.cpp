aba5efafb8d42e8c196f8b3862cf0e3e160bea70
Sebastian Zdziebko <zdziebko.sebastian17@gmail.com>
2025-04-20 19:07:51 +0200

    Added base background

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include "raylib.h"
 
 int main() {
+    // Create new window for game (default fullscreen)
     SetConfigFlags(FLAG_FULLSCREEN_MODE);
     int windowDimensions[2]{GetMonitorWidth(0), GetMonitorHeight(0)};
-
     InitWindow(windowDimensions[0], windowDimensions[1], "Game");
 
+    Texture2D background = LoadTexture("data/map/background.png");
+    Vector2 backgroundPos{0.f, 0.f};
+    float mapScale{4.f};
+
+    // Game loop
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
 
+            DrawTextureEx(background, backgroundPos, 0.f, mapScale, WHITE);
+            
         EndDrawing();
     }
 
