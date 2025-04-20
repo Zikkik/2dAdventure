1c71232233598bf0f74d1e64d59f52b739994f34
Sebastian Zdziebko <zdziebko.sebastian17@gmail.com>
2025-04-20 18:53:07 +0200

    Initialize game window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "raylib.h"
 
 int main() {
-    InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "Game");
+    SetConfigFlags(FLAG_FULLSCREEN_MODE);
+    int windowDimensions[2]{GetMonitorWidth(0), GetMonitorHeight(0)};
+
+    InitWindow(windowDimensions[0], windowDimensions[1], "Game");
 
     while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        EndDrawing();
     }
+
+    CloseWindow();
 }
